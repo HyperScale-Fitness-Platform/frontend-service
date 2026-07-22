e0677ad6deff63e59c9a4582267bafabee8ca0a6
alaaateff <alaaatef3200@gmail.com>
2026-07-22 05:14:53 +0300

    moved all the routes to the app routes file

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,122 +1,137 @@
 import { createBrowserRouter } from 'react-router';
+
 import Layout from '../components/Layout';
+
 import Login from '../features/Login/Login';
 import Register from '../features/register/Register';
+
 import CustomerHomePage from '../features/CustomerHomePage/CustomerHomePage';
+
 import MembershipPlans from '../features/Membership/MembershipPlans';
 import ManageMembership from '../features/Membership/ManageMembership';
+
 import Occupancy from '../features/Occupancy/Occupancy';
+
 import PTPackages from '../features/PTPackages/PTPackages';
-import AdminBooking from '../features/Booking/AdminBooking';
+
+import Booking from '../features/Booking/Booking';
+import AdminBooking from '../features/AdminBooking/AdminBooking';
+
 import AdminMembership from '../features/AdminMembership/AdminMembership';
+
 import ProtectedRoute from '../components/ProtectedRoute';
 
 
 export const router = createBrowserRouter([
 
-  {
-    element: <Layout />,
+    {
+        element: <Layout />,
 
-    children: [
+        children: [
 
-      {
-        path: '/customerHomePage',
+            {
+                path: 'customerHomePage',
 
-        element: (
-          <ProtectedRoute>
-            <CustomerHomePage />
-          </ProtectedRoute>
-        ),
-
-      },
-
-
-      {
-        path: '/membership',
-
-        element: (
-          <ProtectedRoute>
-            <MembershipPlans />
-          </ProtectedRoute>
-        ),
-
-      },
+                element: (
+                    <ProtectedRoute>
+                        <CustomerHomePage />
+                    </ProtectedRoute>
+                ),
+            },
 
 
-      {
-        path: '/manage-membership',
+            {
+                path: 'booking',
 
-        element: (
-          <ProtectedRoute>
-            <ManageMembership />
-          </ProtectedRoute>
-        ),
-
-      },
-
-
-      {
-        path: '/occupancy',
-
-        element: (
-          <ProtectedRoute>
-            <Occupancy />
-          </ProtectedRoute>
-        ),
-
-      },
+                element: (
+                    <ProtectedRoute>
+                        <Booking />
+                    </ProtectedRoute>
+                ),
+            },
 
 
-      {
-        path: '/pt-packages',
+            {
+                path: 'membership',
 
-        element: (
-          <ProtectedRoute>
-            <PTPackages />
-          </ProtectedRoute>
-        ),
-
-      },
-
-
-      {
-        path: '/admin',
-
-        element: (
-          <ProtectedRoute>
-            <AdminBooking />
-          </ProtectedRoute>
-        ),
-
-      },
+                element: (
+                    <ProtectedRoute>
+                        <MembershipPlans />
+                    </ProtectedRoute>
+                ),
+            },
 
 
-      {
-        path: '/admin/membership',
+            {
+                path: 'manage-membership',
 
-        element: (
-          <ProtectedRoute>
-            <AdminMembership />
-          </ProtectedRoute>
-        ),
-
-      },
-
-    ],
-
-  },
+                element: (
+                    <ProtectedRoute>
+                        <ManageMembership />
+                    </ProtectedRoute>
+                ),
+            },
 
 
-  {
-    path: '/login',
-    element: <Login />,
-  },
+            {
+                path: 'occupancy',
+
+                element: (
+                    <ProtectedRoute>
+                        <Occupancy />
+                    </ProtectedRoute>
+                ),
+            },
 
 
-  {
-    path: '/register',
-    element: <Register />,
-  },
+            {
+                path: 'pt-packages',
 
+                element: (
+                    <ProtectedRoute>
+                        <PTPackages />
+                    </ProtectedRoute>
+                ),
+            },
+
+
+            {
+                path: 'admin/booking',
+
+                element: (
+                    <ProtectedRoute>
+                        <AdminBooking />
+                    </ProtectedRoute>
+                ),
+            },
+
+
+            {
+                path: 'admin/membership',
+
+                element: (
+                    <ProtectedRoute>
+                        <AdminMembership />
+                    </ProtectedRoute>
+                ),
+            },
+
+        ],
+
+    },
+
+
+    {
+        path: 'login',
+
+        element: <Login />,
+    },
+
+
+    {
+        path: 'register',
+
+        element: <Register />,
+    },
 
 ]);
